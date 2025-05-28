a54890d7dab0dd4ab91b9df8cc3758ba7f2e5ff8
smagles <lesialist@icloud.com>
2025-05-28 12:29:55 +0200

    fix checkstyle errors

--- src/main/java/com/mate/carsharing/CarSharingAppApplication.java
+++ src/main/java/com/mate/carsharing/CarSharingAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarSharingAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CarSharingAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingAppApplication.class, args);
+    }
 }
